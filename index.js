9a74c104d70272a0d8e6465f5cce2f72cd893795
itsrauldias <39931516+itsrauldias@users.noreply.github.com>
2024-04-17 09:23:11 -0400

    240417-02

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/api', (req, res) => {
+app.get('/', (req, res) => {
     res.send('Hello Express teste!!')
+})
+
+app.get('/teste', (req, res) => {
+    res.json({ message: 'Rota teste' })
 })
 
 app.listen(port, () => {
