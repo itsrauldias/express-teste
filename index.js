e0f104a9a0598b974421cde578aa7975f9fabe86
itsrauldias <39931516+itsrauldias@users.noreply.github.com>
2024-04-17 08:03:30 -0400

    240417-01

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 80
 
-app.get('/', (req, res) => {
+app.get('/api', (req, res) => {
     res.send('Hello Express teste!!')
 })
 
